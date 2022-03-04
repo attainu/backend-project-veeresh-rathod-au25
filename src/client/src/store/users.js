0375b95380d88878155e1a477b38090bf101692f
veeresh-rathod-au25 <85392456+veeresh-rathod-au25@users.noreply.github.com>
2022-03-04 16:18:09 +0530

    Update users.js

--- src/client/src/store/users.js
+++ src/client/src/store/users.js
@@ -1,10 +1,10 @@
 import * as ACTIONS from "../utils/actions";
 const dummyUsers = {
-  dummy1: { username: "sDummy1", lastVisited: "2020-01-02T05:23:59+00:00", admin: true },
-  dummy2: { username: "mDummy2", lastVisited: "2020-03-08T02:23:59+00:00", admin: false },
-  dummy3: { username: "hDummy3", lastVisited: "2020-01-14T04:23:59+00:00", admin: false },
-  dummy4: { username: "dDummy4", lastVisited: "2020-04-12T12:23:59+00:00", admin: true },
-  dummy5: { username: "sDummy5", lastVisited: "2020-01-11T02:23:59+00:00", admin: false }
+  dummy1: { username: "sDummy1", lastVisited: "2022-02-02T05:23:59+00:00", admin: true },
+  dummy2: { username: "mDummy2", lastVisited: "2022-02-08T02:23:59+00:00", admin: false },
+  dummy3: { username: "hDummy3", lastVisited: "2022-02-14T04:23:59+00:00", admin: false },
+  dummy4: { username: "dDummy4", lastVisited: "2022-02-12T12:23:59+00:00", admin: true },
+  dummy5: { username: "sDummy5", lastVisited: "2022-02-11T02:23:59+00:00", admin: false }
 };
 const initalState = { loading: false, payload: dummyUsers, error: false };
 const initalHistoryState = { loading: false, payload: Object.values(dummyUsers), error: false };
