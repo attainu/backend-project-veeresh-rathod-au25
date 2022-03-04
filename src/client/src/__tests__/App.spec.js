c507133c498f315ef457125720deb359c8b8e61b
veeresh-rathod-au25 <85392456+veeresh-rathod-au25@users.noreply.github.com>
2022-03-04 16:15:10 +0530

    Delete src/client/src/__tests__ directory

--- src/client/src/__tests__/App.spec.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import App from "../App";
-import { shallow } from "enzyme";
-
-describe("App.js", () => {
-  it("should match snapshot", () => {
-    expect(shallow(<App />)).toMatchSnapshot();
-  });
-});
